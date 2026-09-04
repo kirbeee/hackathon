1ab2f2bb58ac9335283bd0426f92b985cb16bcc9
kirbeee <pspsean1234@gmail.com>
2026-09-04 16:53:50 +0800

    init blockchain

---
+++ blockChain/README.md
@@ -0,0 +1,20 @@
+1. sudo apt-get update
+
+1.1 `cd hackathon`
+
+2. install solana  
+`curl --proto '=https' --tlsv1.2 -sSfL https://solana-install.solana.workers.dev | bash`
+
+run  validator  
+3. `solana-test-validator`
+
+動作：
+### 發幣
+`solana airdrop 100 9pvghMD6tK7eUb8MQrue6LZ2t1NdTkUQMuQFgVgDGPi1 --url localhost
+Requesting airdrop of 100 SOL`
+
+### 查看錢包餘額
+`solana balance`
+
+### 查看鏈路
+``
---
+++ blockChain/programs/smart-contract/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("B1BL9SDgz2XDkv7yHQYwJoRE5y6sskVgGQZWBzo5W9Ko");
+
+#[program]
+pub mod smart_contract {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
